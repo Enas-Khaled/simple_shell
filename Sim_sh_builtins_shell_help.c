2f06785955a1dc7c3662d681080ad5c43ecd4d69
sayedabdelal <saidmo19734@gmail.com>
2023-05-24 06:35:00 -0700

    fun_shell

--- Sim_sh_builtins_shell_help.c
+++ Sim_sh_builtins_shell_help.c
@@ -1,34 +1,44 @@
 #include "shell.h"
 
 /**
- * Sim_sh_check_for_builtin - this checks if the command
- *	is a builtin or not
- * @args: this is the args (arr of it)
+ * print_shell_info - Print information about the shell.
  *
- * Return: 1 if builtin
- *         0 otherwise
+ * This function prints information about the shell,
+ *	such as its version number and author.
  */
-int Sim_sh_check_for_builtin(char **args)
+void print_shell_info(void)
 {
-	if (!args[0])
-		return (0); /* debug point */
-	if (!_strcmp(args[0], "exit"))
-		Sim_sh_shell_exit(args); /* check */
-	else if (!_strcmp(args[0], "env"))
-		Sim_sh_shell_env();
-	else if (!_strcmp(args[0], "setenv"))
-		Sim_sh_shell_setenv(args);
-	else if (!_strcmp(args[0], "unsetenv"))
-		Sim_sh_shell_unsetenv(args);
-	else if (!_strcmp(args[0], "help")) /* important */
-		Sim_sh_shell_help();
-	else if (!_strcmp(args[0], "cd"))
-		Sim_sh_shell_cd(args);
-	else if (!_strcmp(args[0], "clear")) /* check */
-		Sim_sh_shell_clear(args);
-	else /* default */
-		return (0);
-	/* return */
-	return (1);
+	Sim_sh_puts("\nWelcome to MyShell!\n\n");
+	Sim_sh_puts("Version: 1.0.0\n");
+	Sim_sh_puts("Author: Test\n\n");
 }
 
+/**
+ * print_help_info - Print help information for the shell.
+ *
+ * This function prints information about the available
+ * commands in the shell.
+ */
+void print_help_info(void)
+{
+	Sim_sh_puts("\nAvailable commands:\n\n");
+	Sim_sh_puts("cd [dir]\tChange the current working directory\n");
+	Sim_sh_puts("env\t\tDisplay the environment variables\n");
+	Sim_sh_puts("setenv\t\tSet an environment variable\n");
+	Sim_sh_puts("unsetenv\tUnset an environment variable\n");
+	Sim_sh_puts("exit\tExit the shell\n\n");
+}
+
+/**
+ * Sim_sh_shell_help - Display help information for the shell.
+ *
+ * This function calls the print_shell_info and
+ * print_help_info functions to display information
+ * about the shell and its available commands.
+ */
+
+void Sim_sh_shell_help(void)
+{
+	print_shell_info();
+	print_help_info();
+}
